0c0df5c7ecb3eae3b864b2bbfe323442d14b30e7
creekwater <zhaoyang_wu@foxmail.com>
2022-08-17 23:26:03 +0800

    增加高分屏适配

--- main.cpp
+++ main.cpp
@@ -11,6 +11,9 @@
 
 int main(int argc, char *argv[])
 {
+#if (QT_VERSION >= QT_VERSION_CHECK(5,6,0))
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
     QApplication a(argc, argv);
 
 #if (QT_VERSION <= QT_VERSION_CHECK(5,0,0))
